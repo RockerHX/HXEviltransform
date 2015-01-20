22fc0067395c6539efb80ae58cf8323bc2df8b5c
RockerHX <RockerHX@gmail.com>
2015-01-20 11:30:25 +0800

    Add bd_encrypt and bd_decrypt Methods
    gcj2 to baidu lat,lng

--- c/transform.h
+++ c/transform.h
@@ -6,4 +6,7 @@ void gcj2wgs(double gcjLat, double gcjLng, double *wgsLat, double *wgsLnt);
 void gcj2wgs_exact(double gcjLat, double gcjLng, double *wgsLat, double *wgsLnt);
 double distance(double latA, double lngA, double latB, double lngB);
 
+void bd_encrypt(double gg_lat, double gg_lon, double *bd_lat, double *bd_lon);
+void bd_decrypt(double bd_lat, double bd_lon, double *gg_lat, double *gg_lon);
+
 #endif
