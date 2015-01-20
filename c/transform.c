70d9c11555d59ccb35baf1098fc75beec6321373
RockerHX <RockerHX@gmail.com>
2015-01-20 11:24:05 +0800

    Add bd_encrypt and bd_decrypt Methods
    gcj2 to baidu lat,lng

--- c/transform.c
+++ c/transform.c
@@ -121,3 +121,26 @@ double distance(double latA, double lngA, double latB, double lngB) {
 	double distance = alpha * earthR;
 	return distance;
 }
+
+/**
+ *  GCJ-02 To BD-09
+ */
+const double x_pi = 3.14159265358979324 * 3000.0 / 180.0;
+
+void bd_encrypt(double gg_lat, double gg_lon, double *bd_lat, double *bd_lon)
+{
+    double x = gg_lon, y = gg_lat;
+    double z = sqrt(x * x + y * y) + 0.00002 * sin(y * x_pi);
+    double theta = atan2(y, x) + 0.000003 * cos(x * x_pi);
+    *bd_lon = z * cos(theta) + 0.0065;
+    *bd_lat = z * sin(theta) + 0.006;
+}
+
+void bd_decrypt(double bd_lat, double bd_lon, double *gg_lat, double *gg_lon)
+{
+    double x = bd_lon - 0.0065, y = bd_lat - 0.006;
+    double z = sqrt(x * x + y * y) - 0.00002 * sin(y * x_pi);
+    double theta = atan2(y, x) - 0.000003 * cos(x * x_pi);
+    *gg_lon = z * cos(theta);
+    *gg_lat = z * sin(theta);
+}
